c797c2c5c82c9dafa810b3e1352aa0c91d7f61ae
NikitaLavrentiev <119487200+NikitaLavrentiev@users.noreply.github.com>
2022-12-12 12:00:55 +0300

    pattern created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+    }
+
+    public static void task1() {
+        /**Объявите три массива:
+        Целочисленный массив, заполненный тремя цифрами — 1, 2 и 3 — с помощью ключевого слова new.
+        Массив, в котором можно хранить три дробных числа — 1.57, 7.654, 9.986. Массив сразу заполните значениями.
+         Произвольный массив. Тип и количество данных определите сами.
+         Самостоятельно выберите способ создания массива: с помощью ключевого слова или сразу заполненный элементами. */
+        System.out.println("\nЗадача_1");
+        int [] integer = new int[1, 2,3] ;
+    }
+
+    public static void task2() {
+
+        System.out.println("\nЗадача_2");
+    }
+
+    public static void task3() {
+
+        System.out.println("\nЗадача_3");
+    }
+
+    public static void task4() {
+
+        System.out.println("\nЗадача_4");
+    }
+
+    public static void task5() {
+
+        System.out.println("\nЗадача_5");
+    }
+
+    public static void task6() {
+
+        System.out.println("\nЗадача_6");
+    }
+
+    public static void task7() {
+
+        System.out.println("\nЗадача_7");
     }
 }
